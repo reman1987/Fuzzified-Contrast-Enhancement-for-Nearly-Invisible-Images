f4423374b4cc062db1ca3f1166046477ae97970d
reman1987 <103497576+reman1987@users.noreply.github.com>
2023-06-06 21:51:48 +0530

    Add files via upload

---
+++ Example_code.m
@@ -0,0 +1,12 @@
+%Code for "Fuzzified Contrast Enhancement for Nearly Invisible Images"
+% Degree of fuzzification   m=2;
+% Number of clusters        c=3;
+
+%Cite as    "Fuzzified Contrast Enhancement for Nearly Invisible Images"
+%           IEEE Transactions on Circuits and Systems for Video Technology, 
+%           vol. 32, no. 5, pp. 2802-2813, May 2022
+
+image=imread('cactus.contrast.5.tiff'); %Read input image file (uint8)
+imshow(image);    %Display input image 
+output=FCENII_TCSVT_2022(image); %Call the function FCENII_TCSVT_2022 for result
+figure, imshow(output); %Display result image (uint8)
